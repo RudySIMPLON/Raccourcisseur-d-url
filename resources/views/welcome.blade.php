0e621a8d963d53314b8c9687ef7e2008a4d48581
simplon <rudy__10@hotmail.fr>
2017-05-09 12:20:53 +0200

    faille csrf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,9 @@
 <h1>The best url Shortener out there</h1>
 
 <form action="" method="POST">
-    <input type="text" placeholder="Entrer votre url">
-    <input type="submit" name="url" value="Shorten url">
+     {{csrf_field()}}
+    <input type="text" name="url" placeholder="Entrer votre url">
+    <input type="submit" name="" value="Shorten url">
     
 </form>
 @endsection
