dab8917e920c3651ff74038a3c7ba2f42fd1234f
simplon <rudy__10@hotmail.fr>
2017-05-10 16:39:33 +0200

    ajout layout + titre

--- resources/views/resultat.blade.php
+++ resources/views/resultat.blade.php
@@ -1,4 +1,7 @@
-
+@extends('layouts.base')
+@section('content')
+<h1>Voici votre Url raccourcis</h1>
 <a href="{{config('app.url')}}/{{$shortened}}">
 	{{config('app.url')}}/{{$shortened}}
 </a>
+@stop
