856c79bd549370a65d0e6c4bd71f0130bb1029f5
simplon <rudy__10@hotmail.fr>
2017-05-10 16:49:04 +0200

    mise a jour function down

--- database/migrations/2017_05_10_143615_add_unique_constraint_on_shortened_in_urls_table.php
+++ database/migrations/2017_05_10_143615_add_unique_constraint_on_shortened_in_urls_table.php
@@ -26,7 +26,7 @@ class AddUniqueConstraintOnShortenedInUrlsTable extends Migration
     public function down()
     {
         Schema::table('urls', function (Blueprint $table) {
-            
+            $table->dropUnique("urls_shortened_unique");
         });
     }
 }
