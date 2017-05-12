6f6208d4453ad9376d70d6fb077f03066eb1c511
simplon <rudy__10@hotmail.fr>
2017-05-12 18:03:11 +0200

    changement de la coulleur de mon erreur

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,6 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Url Shortener</title>
+	<style>
+		.errorMsg{
+			color:red;
+			font-size: italic;
+			font-weight: bold;
+		}
+	</style>
 </head>
 <body>
 @yield('content')
