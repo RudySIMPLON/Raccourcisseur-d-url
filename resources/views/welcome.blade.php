6332fffd745587088139d65c0cd11ffac23eaadc
simplon <rudy__10@hotmail.fr>
2017-05-12 18:03:49 +0200

    ajour error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,8 @@
 
 <form action="/" method="POST">
      {{csrf_field()}}
-    <input type="text" name="url" placeholder="Entrer votre url">
+    <input type="text" name="url" placeholder="Entrer votre url" value="{{old('url')}}">
+   {!! $errors->first('url','<p class="errorMsg">Veuillez rentrer une url valide </p>')!!}
     <input type="submit" name="" value="Shorten url">
     
 </form>
